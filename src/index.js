fa4830a1a019af7e0765d3c911adde7700b8048a
jitendravaid99 <114829231+jitendravaid99@users.noreply.github.com>
2024-06-30 20:43:21 +0000

    commit dependecies

---
+++ src/index.js
@@ -0,0 +1,35 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+/*
+import express from "express"
+
+const app = express()
+
+;(async ()=> {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", ()=>{
+            console.log("ERRR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, ()=>{
+            console.log(`Server is listening on port ${process.env.PORT}`)
+        })
+
+    } catch(){
+        console.log("ERROR: ", error)
+        throw error
+    }
+}) ()
+
+*/
